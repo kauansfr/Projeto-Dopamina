b14ed96fafdd4ae6defdbd28bf5431015ccb5d0e
kauansfr <kauan.franca@sptech.school>
2024-05-22 22:47:42 -0300

    Menu da Brain quase finalizado

--- Site Institucional/public/scriptIndex.js
+++ Site Institucional/public/scriptIndex.js
@@ -1,11 +1,19 @@
-function aparecerDopamina() {
     const dopamina = document.querySelector('.box_dopamina');
+    const atividadeFisica = document.querySelector('.box_atividadeFisica');
+    const rotina = document.querySelector('.box_controleRotina');
+    const sobreMim = document.querySelector('.box_sobreMim');
 
+function aparecerDopamina() {
     dopamina.style.display = 'flex';
+    atividadeFisica.style.display = 'flex';
+    rotina.style.display = 'flex';
+    sobreMim.style.display = 'flex';
     setTimeout(animacaoDopamina, 1);
 }
 
 function animacaoDopamina() {
-    const dopamina = document.querySelector('.box_dopamina');
     dopamina.style.transform = 'translateX(-12rem)';
+    atividadeFisica.style.transform = 'translateX(12rem)';
+    rotina.style.transform = 'translateX(-12rem)';
+    sobreMim.style.transform = 'translateX(12rem)';
 }
