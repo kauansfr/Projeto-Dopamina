e74390ec7110d051eb2969951d2358dee96b436d
kauansfr <kauan.franca@sptech.school>
2024-05-23 12:35:00 -0300

    Button "Login" na Home com link para tela equivalente e ajustes nos menus

--- Site Institucional/public/scriptIndex.js
+++ Site Institucional/public/scriptIndex.js
@@ -1,3 +1,4 @@
+    const cerebro = document.querySelector('.brain_img');
     const dopamina = document.querySelector('.box_dopamina');
     const atividadeFisica = document.querySelector('.box_atividadeFisica');
     const rotina = document.querySelector('.box_controleRotina');
@@ -9,6 +10,8 @@ function aparecerDopamina() {
     rotina.style.display = 'flex';
     sobreMim.style.display = 'flex';
     setTimeout(animacaoDopamina, 1);
+
+    desacelerarPulse_Cerebro();
 }
 
 function animacaoDopamina() {
@@ -17,3 +20,8 @@ function animacaoDopamina() {
     rotina.style.transform = 'translateX(-12rem)';
     sobreMim.style.transform = 'translateX(12rem)';
 }
+
+function desacelerarPulse_Cerebro() {
+    cerebro.style.animation = 'pulse_OnClick 2000ms infinite';
+}
+// FUNÇÃO PARA VOLTAR AS DIVS E NO FINAL DO CÓDIGO CHAMAR OUTRA FUNÇÃO COM setTIMOUT PARA DISPLAY NONE
